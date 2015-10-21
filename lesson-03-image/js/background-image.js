af03b0ea20471d0a931b0a859a007ff9da089fe9
jiwonpark <jiwonpark@MacBook-Pro.local>
2015-10-21 16:04:58 +0900

    02-drawing, 03-image

---
+++ lesson-03-image/js/background-image.js
@@ -0,0 +1,26 @@
+/**
+ * http://p5js.org/examples/examples/Image_Background_Image.php
+ */
+
+var bg;
+var y = 0;
+
+function setup() {
+  // The background image must be the same size as the parameters
+  // into the createCanvas() method. In this program, the size of
+  // the image is 720x400 pixels.
+  bg = loadImage("assets/moonwalk.png");
+  createCanvas(720, 400);
+}
+
+function draw() {
+  background(bg);
+
+  stroke(226, 204, 0);
+  line(0, y, width, y);
+
+  y++;
+  if (y > height) {
+    y = 0;
+  }
+}
